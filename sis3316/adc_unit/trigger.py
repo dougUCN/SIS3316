55647229827903b96dd94c39b8b3762e066c2242
dougUCN <douglas.kt.wong@gmail.com>
2024-10-25 14:42:38 -0500

    Add support for MAW tau factor

--- sis3316/adc_unit/trigger.py
+++ sis3316/adc_unit/trigger.py
@@ -19,46 +19,94 @@
 #
 
 from .common import *
-from ..common import * 
+from ..common import *
 from .registers import *
 
+
 class Adc_trigger(object):
-	"""ADC TRIGGER"""
+    """ADC TRIGGER"""
 
-	__slots__ = ('container', 'cid', 'gid', 'idx', 'board') # Restrict attribute list (foolproof).
+    __slots__ = (
+        "container",
+        "cid",
+        "gid",
+        "idx",
+        "board",
+    )  # Restrict attribute list (foolproof).
 
-	def __init__(self, container, gid, cid):
-		""" Trigger configureation. For sum triggers cid is None. """
-		self.gid = gid
-		if cid == None:
-			self.cid = 4	# channel id for sum triggers is 4 (hardwared)
-			self.idx = gid
-		else:
-			self.cid = cid
-			self.idx = self.gid * const.CHAN_PER_GRP + self.cid 
-	
-		self.container = container
-		self.board = container.board
-		
-	_auto_properties = {
-		'maw_peaking_time': Param(0xfFF,  0, FIR_TRIGGER_SETUP_REG, """ Peaking time: number of values to summ."""),
-		'maw_gap_time':     Param(0xfFF, 12, FIR_TRIGGER_SETUP_REG, """ Gap time (flat time)."""),
-		'out_pulse_length': Param(0xFe , 24, FIR_TRIGGER_SETUP_REG, """ External NIM out pulse length (stretched)."""),
-	
-		'threshold':      Param(0xFffFFFF, 0, FIR_TRIGGER_THRESHOLD_REG, """ Trapezoidal threshold value. \nThe full 27-bit running sum + 0x800 0000 is compared to this value to generate trigger."""),
-		'cfd_ena':        Param(0b11,     28, FIR_TRIGGER_THRESHOLD_REG, """ Enable CFD with 50%. 0,1 - disable, 2 -reserved, 3 -enabled."""),
-		'high_suppress_ena':\
-		                  Param(True, 30, FIR_TRIGGER_THRESHOLD_REG, """A trigger will be suppressed if the running sum of the trapezoidal filter goes above the value of the High Energy Threshold register. \nThis mode works only with CFD function enabled ! """),
-		'enable':         Param(True, 31, FIR_TRIGGER_THRESHOLD_REG, """ Enable trigger. """),
-		
-		'high_threshold': Param(0xFffFFFF, 0, FIR_HIGH_ENERGY_THRESHOLD_REG, """ The full 27-bit running sum + 0x800 0000 is compared to the High Energy Suppress threshold value. \n Note 1: use channel invert for negative signals. """),
-		}
-	
-	_conf_params = _auto_properties.keys()
-	dump_conf = common_dump_conf
-	ls = common_ls
-	help = common_help
-	
-	
+    def __init__(self, container, gid, cid):
+        """Trigger configureation. For sum triggers cid is None."""
+        self.gid = gid
+        if cid == None:
+            self.cid = 4  # channel id for sum triggers is 4 (hardwared)
+            self.idx = gid
+        else:
+            self.cid = cid
+            self.idx = self.gid * const.CHAN_PER_GRP + self.cid
+
+        self.container = container
+        self.board = container.board
+
+    # Param fields: mask, offset, reg, doc
+    _auto_properties = {
+        "maw_peaking_time": Param(
+            0xFFF,
+            0,
+            FIR_TRIGGER_SETUP_REG,
+            """ Peaking time: number of values to summ.""",
+        ),
+        "maw_gap_time": Param(
+            0xFFF, 12, FIR_TRIGGER_SETUP_REG, """ Gap time (flat time)."""
+        ),
+        "maw_tau_factor": Param(
+            0x3F, 24, FIR_TRIGGER_SETUP_REG, """ Tau factor. Valid value 0 -> 63"""
+        ),
+        "maw_tau_table": Param(
+            0b11, 30, FIR_TRIGGER_SETUP_REG, """ Select the tau table. 0,1,2,3."""
+        ),
+        "out_pulse_length": Param(
+            0xFE,
+            24,
+            FIR_TRIGGER_SETUP_REG,
+            """ External NIM out pulse length (stretched).""",
+        ),
+        "threshold": Param(
+            0xFFFFFFF,
+            0,
+            FIR_TRIGGER_THRESHOLD_REG,
+            """ Trapezoidal threshold value. \nThe full 27-bit running sum + 0x800 0000
+              is compared to this value to generate trigger.""",
+        ),
+        "cfd_ena": Param(
+            0b11,
+            28,
+            FIR_TRIGGER_THRESHOLD_REG,
+            """ Enable CFD with 50%. 0,1 - disable, 2 -reserved, 3 -enabled.""",
+        ),
+        "high_suppress_ena": Param(
+            True,
+            30,
+            FIR_TRIGGER_THRESHOLD_REG,
+            """A trigger will be suppressed if the running sum of the trapezoidal filter 
+            goes above the value of the High Energy Threshold register. 
+            \nThis mode works only with CFD function enabled ! """,
+        ),
+        "enable": Param(True, 31, FIR_TRIGGER_THRESHOLD_REG, """ Enable trigger. """),
+        "high_threshold": Param(
+            0xFFFFFFF,
+            0,
+            FIR_HIGH_ENERGY_THRESHOLD_REG,
+            """ The full 27-bit running sum + 0x800 0000 is compared 
+            to the High Energy Suppress threshold value. 
+            \n Note 1: use channel invert for negative signals. """,
+        ),
+    }
+
+    _conf_params = _auto_properties.keys()
+    dump_conf = common_dump_conf
+    ls = common_ls
+    help = common_help
+
+
 for name, prop in Adc_trigger._auto_properties.items():
-	setattr(Adc_trigger, name, auto_property(prop, cid_offset = 0x10))
+    setattr(Adc_trigger, name, auto_property(prop, cid_offset=0x10))
